57b6445d6aa852256ec083be9005668aa0c0abcf
Jason Dahl <jrobertdahl@gmail.com>
2021-05-08 15:27:47 -0500

    auth POC

---
+++ public/js/authtest.js
@@ -0,0 +1,40 @@
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    console.log("user is logged in");
+    console.log(user);
+  } else {
+    console.log("no user logged in");
+  }
+});
+
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: function () {
+      // The widget is rendered.
+      // Hide the loader.
+      document.getElementById("loader").style.display = "none";
+    },
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: "popup",
+  signInSuccessUrl: "room.html",
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+  // Terms of service url.
+  tosUrl: "<your-tos-url>",
+  // Privacy policy url.
+  privacyPolicyUrl: "<your-privacy-policy-url>",
+};
+
+// The start method will wait until the DOM is loaded.
+ui.start("#firebaseui-auth-container", uiConfig);
